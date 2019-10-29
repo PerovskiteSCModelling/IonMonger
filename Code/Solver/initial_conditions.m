37354c6f62c869b10bb5a159009865715aac16b1
NicolaCourtier <45851982+NicolaCourtier@users.noreply.github.com>
2019-10-29 17:57:59 +0000

    Update initial_conditions.m

--- Code/Solver/initial_conditions.m
+++ Code/Solver/initial_conditions.m
@@ -11,13 +11,10 @@ function sol_init = initial_conditions(psi0,params,vectors,matrices)
 P_init    = ones(size(x));
 phi_init  = zeros(size(x));
 
-% Compute profiles for the carrier concentrations from quasi-steady BVPs
-p_guess = bvpinit(x',@(x) [kH*x+kE*(1-x)/chi; 0*x]);
-sol = bvp4c(@(x,p) pode(x,p,params),@(pa,pb) pbcs(pa,pb,params),p_guess);
-solx = deval(sol,x'); p_init = solx(1,:)';
-n_guess = bvpinit(x',@(x) [chi*kH*x+kE*(1-x); 0*x]);
-sol = bvp4c(@(x,p) node(x,p,params),@(pa,pb) nbcs(pa,pb,params),n_guess);
-solx = deval(sol,x'); n_init = solx(1,:)';
+% Compute profiles for the carrier concentrations from a quasi-steady BVP
+y_guess = bvpinit(x',@(x) [kH*x+kE*(1-x)/chi; 0*x; chi*kH*x+kE*(1-x); 0*x]);
+sol = bvp4c(@(x,y) yode(x,y,params),@(ya,yb) ybcs(ya,yb,params),y_guess);
+solx = deval(sol,x'); p_init = solx(1,:)'; n_init = solx(3,:)';
 
 % Define uniform profiles for the electric potential and carrier
 % concentrations across the TLs
@@ -47,28 +44,19 @@ end
 
 
 
-% Quasi-steady BVP for the hole concentration
-% y(1) = p(x), y(2) = jp(x) = -Kp*dp/dx
-function dpdx = pode(x,p,params)
-[G, R, Kp, chi] = struct2array(params,{'G','R','Kp','chi'});
-dpdx = [-p(2)/Kp; ...
-        G(x,0)-R(chi*p(1),p(1),1)];
+% Quasi-steady BVP for the carrier concentrations
+% y(1) = p(x), y(2) = jp(x) = -Kp*dp/dx, y(3) = n(x), y(4) = jn(x) = Kn*dn/dx
+function dpdx = yode(x,y,params)
+[G, R, Kp, Kn] = struct2array(params,{'G','R','Kp','Kn'});
+dpdx = [-y(2)/Kp; ...
+        G(x,0)-R(y(3),y(1),1); ...
+        y(4)/Kn; ...
+        -(G(x,0)-R(y(3),y(1),1))];
 end
-function res = pbcs(pa,pb,params)
-[kH, Rl] = struct2array(params,{'kH','Rl'});
-res = [pb(1)-kH; ...
-       pa(2)+Rl(1,pa(1))];
-end
-
-% Quasi-steady BVP for the electron concentration
-% y(1) = n(x), y(2) = jn(x) = Kn*dn/dx
-function dpdx = node(x,p,params)
-[G, R, Kn, chi] = struct2array(params,{'G','R','Kn','chi'});
-dpdx = [p(2)/Kn; ...
-        -(G(x,0)-R(chi*p(1),p(1),1))];
-end
-function res = nbcs(pa,pb,params)
-[kE, Rr] = struct2array(params,{'kE','Rr'});
-res = [pa(1)-kE; ...
-       pb(2)-Rr(pa(1),1)];
+function res = ybcs(ya,yb,params)
+[kH, Rl, kE, Rr] = struct2array(params,{'kH','Rl','kE','Rr'});
+res = [yb(1)-kH; ...
+       ya(2)+Rl(1,ya(1)); ...
+       ya(3)-kE; ...
+       yb(4)+Rr(yb(3),1)];
 end
