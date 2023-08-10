6fea06cd50f1630d4bc217242d073a385e552949
NicolaCourtier <45851982+NicolaCourtier@users.noreply.github.com>
2023-08-10 11:12:13 +0100

    Update plot_nEIS.m

--- Code/Plotting/plot_nEIS.m
+++ Code/Plotting/plot_nEIS.m
@@ -36,6 +36,7 @@ set(0,'defaultLegendInterpreter','latex'); % For latex legends
 M = 2; % marker size
 L = 0.5; % line width
 
+% Set label names for the different orders
 Rlabel = {'Re(Z$_{1}$) / $\Omega$cm$^2$', ...
           'Re(Z$_{2}$) / V$\Omega$cm$^2$', ...
           'Re(Z$_{3}$) / V$^2\Omega$cm$^2$'};
@@ -43,22 +44,30 @@ Xlabel = {'-Im(Z$_{1}$) / $\Omega$cm$^2$', ...
           '-Im(Z$_{2}$) / V$\Omega$cm$^2$', ...
           '-Im(Z$_{3}$) / V$^2\Omega$cm$^2$'};
 
+% Choose line colour
+colour = [126,47,142]/255; % purple
+% colour = [0,0,0]/255; % black
+style = 'o-';
+
 for j = 1:size(X,2)
     figure('Name',['Order ' num2str(j)]);
     % Nyquist plot
     subplot(2,1,1); hold on;
-    plot(R(:,j),-X(:,j),'-or','LineWidth',L,'MarkerSize',M,'MarkerFaceColor','r');
+    plot(R(:,j),-X(:,j),style,'LineWidth',L,'MarkerSize',M, ...
+        'Color',colour,'MarkerFaceColor',colour);
     set(gca,'DataAspectRatio',[1 1 1]);
     ylabel(Xlabel{j});
     xlabel(Rlabel{j});
     % Frequency plots
     subplot(2,2,3); hold on;
-    plot(freqs,R(:,j),'-or','LineWidth',L,'MarkerSize',M,'MarkerFaceColor','r');
+    plot(freqs,R(:,j),style,'LineWidth',L,'MarkerSize',M, ...
+        'Color',colour,'MarkerFaceColor',colour);
     xlabel('Frequency (Hz)');
     ylabel(Rlabel{j});
     set(gca,'Xscale','log');
     subplot(2,2,4); hold on;
-    plot(freqs,-X(:,j),'-or','LineWidth',L,'MarkerSize',M,'MarkerFaceColor','r');
+    plot(freqs,-X(:,j),style,'LineWidth',L,'MarkerSize',M, ...
+        'Color',colour,'MarkerFaceColor',colour);
     xlabel('Frequency (Hz)');
     ylabel(Xlabel{j});
     set(gca,'Xscale','log');
